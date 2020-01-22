af6796fbf6f3d4683e6e7263a38ff2ba57a59f71
Pranav Jain <jainpranav1997@gmail.com>
2020-01-22 15:44:43 -0800

    Lab1 Complete

---
+++ Assignment 1/main.py
@@ -0,0 +1,12 @@
+# File Header
+# Name: Pranav Jain
+# Student Number = 14213029
+# Source File: main.py
+# Course: MECH 550C - Software Design
+# Description: Program to print a string
+# Usage: Run Lab1.exe
+
+print ("Using python I aim to make small apps that \
+supports any automation I carry out in the industry.")
+
+input("\nPress Enter to Exit ")
